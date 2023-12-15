c9f79d774e89005f4fe87c13f7fa076c62de9920
Mailen Lorena Alvarez <m.labarrere2@gmail.com>
2023-12-15 00:18:36 -0300

    street-fighter

---
+++ src/javascript/components/fighterPreview.js
@@ -0,0 +1,51 @@
+import { createElement } from '../helpers/domHelper';
+
+export function createFighterPreview(fighter, position) {
+  const positionClassName = position === 'right' ? 'fighter-preview___right' : 'fighter-preview___left';
+  const fighterElement = createElement({
+    tagName: 'div',
+    className: `fighter-preview___root ${positionClassName}`,
+  });
+
+  // todo: show fighter info (image, name, health, etc.)
+  if (fighter) {
+    fighterElement.appendChild(createFighterImage(fighter));
+    fighterElement.appendChild(createFighterDescription(fighter));
+  }
+
+  return fighterElement;
+}
+
+export function createFighterImage(fighter) {
+  const { source, name } = fighter;
+  const attributes = { 
+    src: source, 
+    title: name,
+    alt: name 
+  };
+  const imgElement = createElement({
+    tagName: 'img',
+    className: 'fighter-preview___img',
+    attributes,
+  });
+
+  return imgElement;
+}
+
+
+export function createFighterDescription(fighter) {
+  const { name, health, attack, defense } = fighter;
+  const descriptionElement = createElement({
+    tagName: 'div',
+    className: 'fighter-preview___description',
+  });
+  
+  descriptionElement.innerHTML = `
+    <h1>${name}</h1>
+    <span>Health: ${health}HP</span></br>
+    <span>Attack: ${attack}</span></br>
+    <defence>Defence: ${defense}</defence></br>
+  `;
+
+  return descriptionElement;
+}
